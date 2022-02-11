44d9b7e1ecdb162ee04d22e8a69423235837b7a7
Matheus Lourenço <matheus503487@gmail.com>
2022-02-10 23:19:52 -0300

    photo style

--- src/views/usuario/UsuarioPerfil.vue
+++ src/views/usuario/UsuarioPerfil.vue
@@ -3,7 +3,7 @@
     <div class="banner_photo">
       <div class="banner">
           <div class="photo">
-
+            
           </div>
       </div>
     </div>
@@ -35,23 +35,28 @@ export default {
 
 .banner{
   margin: 20px auto;
-  width: 60%;
-  height: 200px;
-  border: 1px solid #000;
+  width: 800px;
+  height: 250px;
+  border: 2px solid #000;
   position: relative;
 }
 
 .photo{
   width: 100px;
   height: 100px;
-  border: 1px solid #000;
+  border: 2px solid #000;
+  background: #fff;
   border-radius: 50%;
   position: absolute;
   bottom: 0;
   margin-bottom: -50px;
   margin-left: 10px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
+
 
 .desc{
   margin-top: 60px;
