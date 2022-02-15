8abeaf57996ca6f6341e74ae25d132b98b1dbafb
Matheus Lourenço <matheus503487@gmail.com>
2022-02-15 08:51:47 -0300

    editar user

--- src/views/usuario/EditarUsuario.vue
+++ src/views/usuario/EditarUsuario.vue
@@ -5,32 +5,25 @@
             <nav>
               <h2>Editar Usuário</h2>
                 <form>
-                  <div>
-                    <label for="name">Nome</label>
+                  <label for="name">Nome</label>
+                  <div class="input_editar">
                     <input type="text" v-model="name" name="name">
-                    <div>
-                      <button class="btn editar" @click.prevent="atualizarUsuario">Mudar Nome</button>
-                    </div>
+                    <span>EDITAR</span>
                   </div>
-                  <div>
-                    <label for="name">Email</label>
-                    <input type="email" v-model="email" name="email">
-                    <div>
-                      <button class="btn editar" @click.prevent="atualizarUsuario">Mudar email</button>
-                    </div>
+                  <label for="name">Email</label>
+                  <div class="input_editar">
+                    <input type="email" v-model="email" name="email" class="input_editar">
+                    <span>EDITAR</span>
                   </div>
-                 <div>
-                    <label for="password">Senha Atual</label>
-                    <input type="password" name="password" v-model="senhaAtual">
+                  <label>Senha</label>
+                 <div class="input_editar">
+                    <input type="password" v-model="password" class="input_editar">
+                    <span>EDITAR</span>
                  </div>
-                  <div>
-                    <label for="password">Nova Senha</label>
-                    <input type="password" name="password">
-                    <div>
-                      <button class="btn editar" @click.prevent="senhaCheck">Mudar senha</button>
-                    </div>
-                  </div>
                 </form>
+                  <div>
+                    <button class="btn editar" @click.prevent="atualizarUsuario">Atualizar Dados</button>
+                  </div>
             </nav>
         </div>
     </section>
@@ -58,19 +51,24 @@ export default {
   methods:{
     atualizarUsuario(){
       api.put(`/users/${this.$store.state.user.id}`, this.$store.state.user).then(()=>{
-        this.$store.dispatch("getUsuario")
+        this.$store.dispatch("getUser")
         this.$router.push({name: "usuario"})
       }).catch(erro =>{
         console.log(erro)
       })
     },
     senhaCheck(){
-      if(this.senhaAtual === this.$store.state.user.password ){
+      if(this.senhaAtual === this.$store.state.user.password){
         this.atualizarUsuario();
       }else{
         console.log('senha errada')
       }
     },
+  },
+  watch:{
+    senhaAtual(){
+      this.senhaCheck()
+    }
   }
 }
 </script>
@@ -78,18 +76,39 @@ export default {
 <style scoped>
 
   .container_infos{
-  width: 450px;
+    width: 100%;
+    max-width: 750px;
+    display: flex;
+    justify-content: center;
   }
 
   form{
     display: flex;
     flex-direction: column;
     margin: 10px 0;
+    width: 100%;
+    max-width: 700px;
   }
 
   label{
     font-size: 1.2rem;
     margin: 5px 0;
+  }
+
+  .input_editar{
+    display: flex;
+    align-items: center;
+  }
+
+  .input_editar span{
+    cursor: pointer;
+    margin-left: -70px;
+    padding: 15px 5px;
+    color: rgb(88, 88, 88);
+  }
+
+  .input_editar input{
+    padding-right: 90px;
   }
 
 </style>
