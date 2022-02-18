d19173bf6ae77cf45a6507d2895c2be86d89d941
Matheus Lourenço <matheus503487@gmail.com>
2022-02-18 11:21:11 -0300

    vuelidate

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuelidate from "vuelidate"
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuelidate,
   store,
   render: h => h(App)
 }).$mount('#app')
